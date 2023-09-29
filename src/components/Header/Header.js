0f907ec19438695ce4fd01743741bea996cc2409
barondevke <ndabasteve1@gmail.com>
2023-09-29 11:42:27 +0300

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,60 @@
-import React from 'react'
-import {Container,Nav,NavDropdown, Navbar } from 'react-bootstrap';
-import '../../pages/styles.css'
+import React from "react";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import "../../pages/styles.css";
 import { Link, animateScroll as scroll } from "react-scroll";
 import { scroller } from "react-scroll";
 const Header = () => {
   return (
     <div>
-       <header>
-      <Navbar variant='dark' expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+      <header>
+        <Navbar variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home"></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link
+                  onClick={() =>
+                    scroller.scrollTo("background-picture", {
+                      smooth: true,
+                      offset: -70,
+                      duration: 500,
+                    })
+                  }
+                >
+                  Home
+                </Nav.Link>
 
-          <Nav.Link onClick={() => scroller.scrollTo('background-picture',{smooth: true,
-                offset: -70,
-                duration: 500,
-        })}>Home</Nav.Link>
-        
-        <Nav.Link onClick={() => scroller.scrollTo('Projectscontainer',{smooth: true,
-                offset: -20,
-                duration: 100,
-        })}>Projects</Nav.Link>
+                <Nav.Link
+                  onClick={() =>
+                    scroller.scrollTo("Projectscontainer", {
+                      smooth: true,
+                      offset: -20,
+                      duration: 100,
+                    })
+                  }
+                >
+                  Projects
+                </Nav.Link>
 
-        <Nav.Link onClick={() => scroller.scrollTo('contactPage',{smooth: true,
-                offset: -30,
-                duration: 100,
-        })}>Contact Me</Nav.Link>
-            
-          
-           
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+                <Nav.Link
+                  onClick={() =>
+                    scroller.scrollTo("contactPage", {
+                      smooth: true,
+                      offset: -30,
+                      duration: 100,
+                    })
+                  }
+                >
+                  Contact Me
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/homepage/Projects.js
+++ src/pages/homepage/Projects.js
@@ -1,75 +1,69 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { Image, Col, Row,Container } from 'react-bootstrap'
-import "../../pages/styles.css"
-
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Image, Col, Row, Container, NavLink } from "react-bootstrap";
+import "../../pages/styles.css";
 
 const Projects = () => {
   return (
-    <div className='Projectscontainer'>
-      
-        <h1>Projects</h1>
-        <Row className='projectRow'>
-        <Col sm={6} >
-            <Card style={{ width: '40rem' }}>
-      <Card.Img variant="top" src="" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card> 
-    </Col>
-            <Col sm={6} className = 'project-description' > <h2>  Project Name</h2>
-             <br/> <p className='project-Info'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-            software like Aldus PageMaker including versions of Lorem Ipsum</p>  </Col>
-        </Row>
-        
-        <Row className='projectRow'>
-            <Col sm={6} >
-            <Card style={{ width: '40rem' }}>
-      <Card.Img variant="top" src="" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card> 
-    </Col>
-            <Col sm={6} className = 'project-description' > <h2>  Project Name</h2>
-             <br/> <p className='project-Info'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-            software like Aldus PageMaker including versions of Lorem Ipsum</p>  </Col>
-        </Row>
-        
-        <Row className='projectRow'>
-        <Col sm={6} >
-            <Card style={{ width: '40rem' }}>
-      <Card.Img variant="top" src="" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card> 
-    </Col>
-            <Col sm={6} className = 'project-description' > <h2>  Project Name</h2>
-             <br/> <p className='project-Info'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-            software like Aldus PageMaker including versions of Lorem Ipsum</p>  </Col>
-        </Row>
-        
-    
-    </div>
-  )
-}
+    <div className="Projectscontainer">
+      <h1>Projects</h1>
+      <Row className="projectRow">
+        <Col sm={6}>
+          <Card style={{ width: "40rem" }}>
+            <Card.Img
+              variant="top"
+              src={require("./img/Screenshot 2023-09-29 105845.png")}
+            />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <Card.Text>Click below to visit the deployed website.</Card.Text>
+              <a href="https://eurekahealthsolutions.netlify.app/">
+                <Button variant="primary">Visit website</Button>
+              </a>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={6} className="project-description">
+          {" "}
+          <h2> Eureka Health frontend website.</h2>
+          <br />{" "}
+          <p className="project-Info">
+            This project uses React to build a simple informative website that
+            clients can use to understand the business and get necessary
+            information details.
+          </p>{" "}
+        </Col>
+      </Row>
+      <hr />
 
-export default Projects
+      <Row className="projectRow">
+        <Col sm={6}>
+          <Card style={{ width: "40rem" }}>
+            <Card.Img
+              variant="top"
+              src={require("./img/Screenshot (32).png")}
+            />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <Card.Text>This hasn't been deployed yet.</Card.Text>
+              <Button variant="primary">Unavailable</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={6} className="project-description">
+          {" "}
+          <h2> AdInfinte website</h2>
+          <br />{" "}
+          <p className="project-Info">
+            This project utilizes various technologies. This includes React for
+            the frontend and Node.js for the backend. For the database I used
+            MongoDB which is a great solution for storing and manipulating data.
+          </p>{" "}
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Projects;
