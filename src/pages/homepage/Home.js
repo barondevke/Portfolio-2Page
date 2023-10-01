445ba8f0a87eac99b4b38ce281ce9d7dd87c5866
barondevke <ndabasteve1@gmail.com>
2023-10-01 18:33:39 +0300

    changed home message

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import { Image, Col, Row,Container } from 'react-bootstrap'
-import "../../pages/styles.css"
+import React from "react";
+import Header from "../../components/Header/Header";
+import { Image, Col, Row, Container } from "react-bootstrap";
+import "../../pages/styles.css";
 
 const Home = () => {
   return (
     <div>
-     <header>
-     <Header/>
-    </header>   
-    <div className='background-picture'>
-            <Row className='px-4'>
-                <Col sm={6} className="hellotxt"> <p>Hello, I'm Steve Baron. </p> <h1><em>A full-stack developer.</em></h1></Col>
-                <Col sm={6} className="helloimage"><div className='helloimage'></div></Col>
-            </Row>
-        </div>
-        
-        
-  </div>
+      <header>
+        <Header />
+      </header>
+      <div className="background-picture">
+        <Row className="px-4">
+          <Col sm={6} className="hellotxt">
+            {" "}
+            <p>Hello, I'm Steve Baron. </p>{" "}
+            <h1>
+              <em>You're next full-stack developer.</em>
+            </h1>
+          </Col>
+          <Col sm={6} className="helloimage">
+            <div className="helloimage"></div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};
 
-
-
-  )
-}
-
-export default Home
+export default Home;
